8cde3272fa32622777cd9d0649a2747fabea2df7
Alexey Zhelezov <zhelezovalexey@gmail.com>
2019-10-22 00:16:56 +0200

    Lifted state TaskTile in Todoey

--- todoey/lib/screens/add_task_screen.dart
+++ todoey/lib/screens/add_task_screen.dart
@@ -1,15 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:todoey_flutter/models/task.dart';
-import 'package:provider/provider.dart';
-import 'package:todoey_flutter/models/task_data.dart';
 
 class AddTaskScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    String newTaskTitle;
-
     return Container(
-      color: Color(0xff757575),
+      color: Color(0xFF757575),
       child: Container(
         padding: EdgeInsets.all(20.0),
         decoration: BoxDecoration(
@@ -26,30 +21,22 @@ class AddTaskScreen extends StatelessWidget {
               'Add Task',
               textAlign: TextAlign.center,
               style: TextStyle(
-                fontSize: 30.0,
                 color: Colors.lightBlueAccent,
+                fontSize: 30.0,
               ),
             ),
             TextField(
               autofocus: true,
               textAlign: TextAlign.center,
-              onChanged: (newText) {
-                newTaskTitle = newText;
-              },
             ),
             FlatButton(
               child: Text(
                 'Add',
-                style: TextStyle(
-                  color: Colors.white,
-                ),
+                style: TextStyle(color: Colors.white),
               ),
               color: Colors.lightBlueAccent,
-              onPressed: () {
-                Provider.of<TaskData>(context).addTask(newTaskTitle);
-                Navigator.pop(context);
-              },
-            ),
+              onPressed: () {},
+            )
           ],
         ),
       ),
--- todoey/lib/widgets/task_tile.dart
+++ todoey/lib/widgets/task_tile.dart
@@ -1,31 +1,42 @@
 import 'package:flutter/material.dart';
 
-class TaskTile extends StatelessWidget {
-  final bool isChecked;
-  final String taskTitle;
-  final Function checkboxCallback;
-  final Function longPressCallback;
+class TaskTile extends StatefulWidget {
+  @override
+  _TaskTileState createState() => _TaskTileState();
+}
 
-  TaskTile(
-      {this.isChecked,
-      this.taskTitle,
-      this.checkboxCallback,
-      this.longPressCallback});
+class _TaskTileState extends State<TaskTile> {
+  bool isChecked = false;
 
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      onLongPress: longPressCallback,
       title: Text(
-        taskTitle,
+        'This is a task',
         style: TextStyle(
             decoration: isChecked ? TextDecoration.lineThrough : null),
       ),
-      trailing: Checkbox(
-        activeColor: Colors.lightBlueAccent,
-        value: isChecked,
-        onChanged: checkboxCallback,
-      ),
+      trailing: TaskCheckbox(isChecked, (bool checkboxState) {
+        setState(() {
+          isChecked = checkboxState;
+        });
+      }),
+    );
+  }
+}
+
+class TaskCheckbox extends StatelessWidget {
+  final bool checkboxState;
+  final Function toggleCheckboxState;
+
+  TaskCheckbox(this.checkboxState, this.toggleCheckboxState);
+
+  @override
+  Widget build(BuildContext context) {
+    return Checkbox(
+      value: checkboxState,
+      activeColor: Colors.lightBlueAccent,
+      onChanged: toggleCheckboxState,
     );
   }
 }
